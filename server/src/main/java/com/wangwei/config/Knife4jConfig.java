bc8cc4ded8a8dcb3a60e26ec012d6c029a2633a1
lijiangtao <3530812934@qq.com>
2026-03-13 17:18:59 +0800

    feat: 配置knife4j接口文档的 admin 和 user

--- server/src/main/java/com/wangwei/config/Knife4jConfig.java
+++ server/src/main/java/com/wangwei/config/Knife4jConfig.java
@@ -3,6 +3,7 @@ package com.wangwei.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +20,23 @@ public class Knife4jConfig {
                         .contact(new Contact()
                                 .name("L学长")
                                 .email("3530812934@qq.com")));
+    }
+
+    // admin模块
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("admin接口")
+                .packagesToScan("com.wangwei.controller.admin")
+                .build();
+    }
+
+    // user模块
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("user接口")
+                .packagesToScan("com.wangwei.controller.user")
+                .build();
     }
 }
